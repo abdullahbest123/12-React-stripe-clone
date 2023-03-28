4e46ecd613130a3bc55f53af0bd560f36f7695d6
abdullahbest123 <abdullahbest19@gmail.com>
2023-03-28 20:45:52 +0500

    Updates

---
+++ precache-manifest.750b879b3eaac46ba7b903295e20b4e9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92c19ec28487bee6868e9a0d881e23b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa03dc7b514199f9606f",
+    "url": "/static/css/main.ebbd086f.chunk.css"
+  },
+  {
+    "revision": "d777f20984e7404383e5",
+    "url": "/static/js/2.5d22bc08.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.5d22bc08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa03dc7b514199f9606f",
+    "url": "/static/js/main.5c69e09e.chunk.js"
+  },
+  {
+    "revision": "35d8ea8e0de0fd66811a",
+    "url": "/static/js/runtime-main.bb80f66d.js"
+  },
+  {
+    "revision": "8596ab4a635cf6b6ea535b15bdc0dc67",
+    "url": "/static/media/hero.8596ab4a.svg"
+  },
+  {
+    "revision": "1090473dd728fcc17033df70118fa668",
+    "url": "/static/media/logo.1090473d.svg"
+  },
+  {
+    "revision": "58d7e3d689c0cb8eb365b86046ecdce8",
+    "url": "/static/media/phone.58d7e3d6.svg"
+  }
+]);
